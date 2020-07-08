395169f5d3c893b30c902fe2c7b561725edc113b
emabytes <emadavor22@gmail.com>
2020-07-08 21:01:56 +0200

    added classes

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
-console.log(document.getElementById("element").classList);
+// function tinyChange() {
+//     document.getElementById("element").classList.add("small");
+//     document.getElementById("element").style.color = "green";
+//     document.getElementById("element").style.boxShadow = "0px 0px 0px 5px grey";
+//     document.getElementById("element").style.fontFamily = "Karla";
+//     document.getElementById("element").style.textShadow = "none";
+//     document.getElementById("element").style.transitionProperty = "none";
+//     document.getElementById("element").style.transitionDuration = "none";
+// }
+
+// function middleChange() {
+//     document.getElementById("element").classList.toggle("small");
+//     document.getElementById("element").style.color = "green";
+//     document.getElementById("element").style.boxShadow = "0px 0px 0px 10px grey";
+//     document.getElementById("element").style.fontFamily = "Karla";
+//     document.getElementById("element").style.textShadow = "none";
+
+// }
+
+// function crazyChange() {
+//     document.getElementById("element").classList.toggle("small");
+//     document.getElementById("element").style.boxShadow = "0px 0px 0px 20px green";
+//     document.getElementById("element").style.textShadow = "2px 2px green";
+//     document.getElementById("element").style.fontFamily = "Notable";
+//     document.getElementById("element").style.letterSpacing = "3px";
+//     document.getElementById("element").style.transitionProperty = "color";
+//     document.getElementById("element").style.transitionDuration = "3s";
+//     document.getElementById("element").style.color = "gold";
+// }
+
 
 function tinyChange() {
-    document.getElementById("element").classList.add("small");
-    document.getElementById("element").style.color = "green";
-    document.getElementById("element").style.boxShadow = "0px 0px 0px 5px grey";
-    document.getElementById("element").style.fontFamily = "Karla";
-    document.getElementById("element").style.textShadow = "none";
-    document.getElementById("element").style.transitionProperty = "none";
-    document.getElementById("element").style.transitionDuration = "none";
+    document.getElementById("element").classList.remove("middle")
+    document.getElementById("element").classList.remove("big")
+    document.getElementById("element").classList.toggle("small")
 }
 
 function middleChange() {
-    document.getElementById("element").classList.toggle("small");
-    document.getElementById("element").style.color = "green";
-    document.getElementById("element").style.boxShadow = "0px 0px 0px 10px grey";
-    document.getElementById("element").style.fontFamily = "Karla";
-    document.getElementById("element").style.textShadow = "none";
+    document.getElementById("element").classList.remove("big")
+    document.getElementById("element").classList.remove("small")
+    document.getElementById("element").classList.toggle("middle")
 
 }
-
 function crazyChange() {
-    document.getElementById("element").classList.toggle("small");
-    document.getElementById("element").style.boxShadow = "0px 0px 0px 20px green";
-    document.getElementById("element").style.textShadow = "2px 2px green";
-    document.getElementById("element").style.fontFamily = "Notable";
-    document.getElementById("element").style.letterSpacing = "3px";
-    document.getElementById("element").style.transitionProperty = "color";
-    document.getElementById("element").style.transitionDuration = "3s";
-    document.getElementById("element").style.color = "gold";
+    document.getElementById("element").classList.remove("small")
+    document.getElementById("element").classList.remove("middle")
+    document.getElementById("element").classList.toggle("big")
 }
-
-
